071c56fffc0490a33ae17fecfc01aceee537d339
Franck STAUFFER <franck.stauffer@rbs.fr>
2011-06-16 15:19:01 +0200

    [DEPRECATE] frontoffice edition is now deprecated

---
+++ lib/phptal/PHPTAL_CHANGE.class.php
@@ -0,0 +1,15 @@
+<?php
+class compatibilityos_PHPTAL_CHANGE
+{
+	/**
+	 * @param PHPTAL_Namespace_CHANGE $namespaceCHANGE
+	 */
+	public static function addAttributes($namespaceCHANGE)
+	{
+            $namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeSurround('docattr', 31));
+            $namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeSurround('propattr', 31));
+            $namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeReplace('edit', 31));
+            $namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeSurround('create', 31));
+        }
+
+}
