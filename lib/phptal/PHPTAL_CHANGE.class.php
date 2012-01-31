4536cf8c9067cae1080c1cb51ebdc429c5243906
Gaël PORT <gael.port@rbs.fr>
2012-01-31 11:45:59 +0100

    [REFACTOR] Move old menu blocks and change:menu PHPTAL extension to compatibilityos.

--- lib/phptal/PHPTAL_CHANGE.class.php
+++ lib/phptal/PHPTAL_CHANGE.class.php
@@ -6,10 +6,14 @@ class compatibilityos_PHPTAL_CHANGE
 	 */
 	public static function addAttributes($namespaceCHANGE)
 	{
-            $namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeSurround('docattr', 31));
-            $namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeSurround('propattr', 31));
-            $namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeReplace('edit', 31));
-            $namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeSurround('create', 31));
-        }
-
+		$namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeSurround('docattr', 31));
+		$namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeSurround('propattr', 31));
+		$namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeReplace('edit', 31));
+		$namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeSurround('create', 31));
+		
+		$namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeSurround('menu', 9));
+		$namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeSurround('menuitem', 9));
+		
+		$namespaceCHANGE->addAttribute(new PHPTAL_NamespaceAttributeSurround('currentlink', 30));
+	}
 }
