d302241f2ec519ae277732137e0ed5e8f26eb3f3
Gaël PORT <gael.port@rbs.fr>
2012-04-24 14:20:57 +0200

    [DEPRECATE] Move old ProcessClosed block from order to compatibilityos.

---
+++ lib/order/lib/blocks/BlockProcessClosedAction.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @deprecated
+ */
+class order_BlockProcessClosedAction extends website_TaggerBlockAction
+{
+	/**
+	 * @deprecated
+	 */
+	public function execute($request, $response)
+	{
+		return website_BlockView::SUCCESS;	
+	}
+}
